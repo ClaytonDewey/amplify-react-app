e34957aeda5169dbbca3bfd315dc9cc5b57330ff
ClaytonDewey <clay@dryadmedia.com>
2023-01-25 12:18:44 -0600

    Make API call and display data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+// Import useState and useEffect hooks from React
+import React, { useEffect, useState } from 'react';
+
+// Import the API category from AWS Amplify
+import { API } from 'aws-amplify';
+
 import './App.css';
 
-function App() {
+const App = () => {
+  // Create coins variable and set to empty array
+  const [coins, updateCoins] = useState([]);
+
+  // Defin function to call API
+  const fetchCoins = async () => {
+    const data = await API.get('cryptoapi', '/coins');
+    updateCoins(data.coins);
+  };
+
+  // Call fetchCoins function when component loads
+  useEffect(() => {
+    fetchCoins();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {coins.map((coin, index) => (
+        <div key={index}>
+          <h2>
+            {coin.name} - {coin.symbol}
+          </h2>
+          <h5>${coin.price_usd}</h5>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
