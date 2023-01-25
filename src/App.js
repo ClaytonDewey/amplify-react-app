6c7653c1e1850ae4fdcb41ee9784eb772971ba55
ClaytonDewey <clay@dryadmedia.com>
2023-01-25 12:48:35 -0600

    Update API call, add user inputs

--- src/App.js
+++ src/App.js
@@ -9,10 +9,22 @@ import './App.css';
 const App = () => {
   // Create coins variable and set to empty array
   const [coins, updateCoins] = useState([]);
+  // Create addditional state to hold user input for limit and start properties
+  const [input, updateInput] = useState({ limit: 5, start: 0 });
 
-  // Defin function to call API
+  // Create a new function to allow users to update the input values
+  const updateInputValues = (type, value) => {
+    updateInput({ ...input, [type]: value });
+  };
+
+  // Update fetchCoins function to use limit and start properties
   const fetchCoins = async () => {
-    const data = await API.get('cryptoapi', '/coins');
+    const { limit, start } = input;
+    const data = await API.get(
+      'cryptoapi',
+      `/coins?limit=${limit}&start=${start}`
+    );
+
     updateCoins(data.coins);
   };
 
@@ -23,6 +35,18 @@ const App = () => {
 
   return (
     <div className='App'>
+      {/* Add input fields to the UI for user input */}
+      <input
+        onChange={(e) => updateInputValues('limit', e.target.value)}
+        placeholder='limit'
+      />
+      <input
+        placeholder='start'
+        onChange={(e) => updateInputValues('start', e.target.value)}
+      />
+
+      {/* Add button to the UI to give user the option to call the API */}
+      <button onClick={fetchCoins}>Fetch Coins</button>
       {coins.map((coin, index) => (
         <div key={index}>
           <h2>
